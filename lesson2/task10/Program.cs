11edfad9e8c4c4d2466cea5183e0b8ff3bb4920c
Danil Selifanov <dg.selifanov@yandex.ru>
2023-05-01 12:42:30 +0500

    add task10 lesson2

---
+++ lesson2/task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000){
+    string str = Convert.ToString(number);
+    Console.Write($"Вторая цифра введенного числа {number} - {str[1]}");
+}
+else {
+    Console.Write("Вы ввели не трехзначное число");
+}
